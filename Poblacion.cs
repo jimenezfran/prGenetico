d92f59288dc4a1abca6765fff3ba695fd7f5daa2
Rosana Ruiz <totemika@gmail.com>
2013-11-04 01:12:09 +0100

    V1

---
+++ Poblacion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prGenetico
+{
+    class Poblacion
+    {
+        private Individuo[] individuos;
+
+
+        int NumIndividuos { get { return individuos.Length; } }
+        Individuo this[int i] { get { return individuos[i]; } }
+
+        public Individuo MejorIndividuo()
+        {
+            int mayorFitness = 0;
+            for (int i = 0; i < individuos.Length; i++)
+            {
+                if (individuos[i].Fitness > mayorFitness) { mayorFitness = i; }
+            }
+
+            return individuos[mayorFitness];
+        }
+
+        public Poblacion(int tP, int tI, IProblema pr)
+        {
+            individuos = new Individuo[tP];
+
+            for (int i = 0; i < individuos.Length; i++)
+            {
+                individuos[i] = new Individuo(tI, pr);
+            }
+        }
+
+        public void Reemplaza(Individuo individuo)
+        {
+            int menorFitness = individuo.Cromosoma.Longitud;
+            for (int i = 0; i < individuos.Length; i++)
+            {
+                if (individuos[i].Fitness < menorFitness) { menorFitness = i; }
+            }
+
+            if (individuos[menorFitness].Fitness < individuo.Fitness)
+            {
+                individuos[menorFitness] = individuo;
+            }
+
+        }
+    }
+}
